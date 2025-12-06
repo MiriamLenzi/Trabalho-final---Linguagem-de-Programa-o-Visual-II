d584106210aaa5b07cf3a6e24b69033ee8124b64
RaianeAlves15 <raianealvesmacedo15@gmail.com>
2025-12-06 20:14:44 -0300

    Pasta Repositories: Adiciona repositório de filmes com SQLite
    Implementa camada de dados para filmes:
    - CRUD completo (Create, Read, Update, Delete)
    - Banco SQLite com criação automática
    - Busca por ID local e TMDb ID
    - Proteção contra SQL injection

---
+++ CatalogoDeFilmes/CatalogoDeFilmes/Repositories/IFilmeRepository.cs
@@ -0,0 +1,13 @@
+﻿using CatalogoDeFilmes.Models;
+
+namespace CatalogoDeFilmes.Repositories;
+
+public interface IFilmeRepository
+{
+    Task<List<Filme>> ListAsync();
+    Task<Filme?> GetByIdAsync(int id);
+    Task<Filme?> GetByTmdbIdAsync(int tmdbId);
+    Task CreateAsync(Filme filme);
+    Task UpdateAsync(Filme filme);
+    Task DeleteAsync(int id);
+}
